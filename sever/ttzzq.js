335947cb193d87a9fe0ab8ef2340118f6ea240e6
ottocsb <w2606662632@gmail.com>
2024-04-20 21:24:08 +0800

    ttzzq

--- sever/ttzzq.js
+++ sever/ttzzq.js
@@ -13,5 +13,4 @@ hostname = g642k8.laf.run
 let obj = JSON.parse($response.body);
 console.log(obj);
 //saveDataStr 是个字符串我需要匹其中的'diamond\'到下一个逗号之间的数字 修改成99999
-obj.data.saveDataStr = obj.data.saveDataStr.replace(/diamond\\":\d+/g, 'diamond\\":99999');
 $done({ body: JSON.stringify(obj) });
